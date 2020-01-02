007b3b3f764dc1125b3e1ad9e49291d1d278dd63
Donna Hogan <ddhogan@users.noreply.github.com>
2020-01-02 16:08:50 -0500

    [ODP-763] Migrate to using access token

--- src/API/config.js
+++ src/API/config.js
@@ -1,24 +1,12 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'test') {
   module.exports = {
-    url: "https://api.trade.gov/v1/web_documents/search.json",
-    apiKey: "d0lmowK8tvxPixAfm9sKOd_f",
-  }
-} else if (process.env.NODE_ENV === 'staging') {
-  module.exports = {
-    url: "https://api.govwizely.com/v1/web_documents/search.json",
-    apiKey: "FqEELAPpt-UDPBxpoo5onvku",
-  }
-} else if (process.env.NODE_ENV === 'test'){
-  module.exports = {
-    /* move the json file to /public first */
+    /* move the json file to `/public` first */
     url: "./Drupal_Intrasearch-export.json",
     apiKey: "test",  
   }
 } else {
   module.exports = {
-    url: "https://api.trade.gov/v1/web_documents/search.json",
-    apiKey: "d0lmowK8tvxPixAfm9sKOd_f",
-    // url: "./Drupal_Intrasearch-export.json",
-    // apiKey: "test",  
+    url: "https://api.trade.gov/gateway/v1/web_documents/search",
+    accessToken: "b0045391-2ef8-3049-a215-f78b7716f045",
   }
 }
